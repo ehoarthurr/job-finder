e8f6e3db38d72eeaceddd54bd3f0ccc42d9cae95
Arthur Danielson <arthurdanielson7@gmail.com>
2025-01-15 19:07:37 -0400

    criando a rota para adicionar registros

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db.authenticate()
@@ -21,3 +25,6 @@ db.authenticate()
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
---
+++ routes/jobs.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/Job");
+
+router.get("/test", (req, res) => {
+  console.log("deu certo");
+});
+
+// adicionar job via POST
+router.post("/add", (req, res) => {
+  let { title, description, salary, company, email, new_job } = req.body;
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job,
+  })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
