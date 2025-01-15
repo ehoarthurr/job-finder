9c44549cd5c8950e099e4da58f229631ec7ade90
Arthur Danielson <arthurdanielson7@gmail.com>
2025-01-15 10:09:30 -0400

    conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const db = require("./db/connection");
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
-  console.log(`O Express está rodando na porta ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar:", err);
+  });
+
+// routes
 app.get("/", (req, res) => {
-  res.send("Hello World! 3");
+  res.send("Hello World!");
 });
